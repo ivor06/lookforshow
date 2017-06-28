911c43e9f4b53ba158bb3037c1ed4bfec9797ce1
Michael M <ivor06@yandex.ru>
2017-06-28 14:37:28 +0500

    Server: Geo routes added.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,11 +1,15 @@
 import * as Router from "koa-router";
 
 import {authRoutes, authAllowedMethods} from "./auth/index";
+import {userRoutes, userAllowedMethods} from "./user/index";
+import {geoRoutes, geoAllowedMethods} from "./geo/index";
 
 const router = new Router();
 
 router
-    .use("/auth", authRoutes(), authAllowedMethods());
+    .use("/auth", authRoutes(), authAllowedMethods())
+    .use("/user", userRoutes(), userAllowedMethods())
+    .use("/geo", geoRoutes(), geoAllowedMethods());
 
 const
     routes = () => router.routes(),
