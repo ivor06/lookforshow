d2deec8e9ad30415037fe50ec272514bb1a604d3
Michael M <ivor06@yandex.ru>
2017-07-26 11:45:27 +0400

    Client: InputCheckBox added.

--- client/src/components/InputCheckBox/InputCheckBox.tsx
+++ client/src/components/InputCheckBox/InputCheckBox.tsx
@@ -3,18 +3,16 @@ import * as React from "react";
 import {InputCheckBoxProps} from "./types";
 
 const InputCheckBox = ({name, label, checked, error, onChange}: InputCheckBoxProps) => <div className="form-group">
-    <div className="form-group">
-        <label htmlFor={name}>{label}</label>
-        <div className="field display-inline">
-            <input
-                type="checkbox"
-                name={name}
-                checked={checked}
-                onChange={onChange}
-                className="form-control inline-20px">
-            </input>
-            {error && <div className="alert alert-danger">{error}</div>}
-        </div>
+    <label htmlFor={name}>{label}</label>
+    <div className="field display-inline">
+        <input
+            type="checkbox"
+            name={name}
+            checked={checked}
+            onChange={onChange}
+            className="form-control inline-20px">
+        </input>
+        {error && <div className="alert alert-danger">{error}</div>}
     </div>
 </div>;
 
